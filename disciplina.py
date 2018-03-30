519b3411a5e069ee37c6b8fe0edb441665625baa
tr0v40 <34144357+tr0v40@users.noreply.github.com>
2018-03-29 22:21:11 -0300

    Create disciplina.py

---
+++ disciplina.py
@@ -0,0 +1,27 @@
+class Disciplina():
+    def __init__(self, nome = ' ' , cargaHoraria = 0, valor = 0, professor = ' ')
+        self.__nome = nome
+        self.__cargaHoraria = cargaHoraria
+        self.__valor = valor
+        self.professor = professor
+
+    def getNome():
+        return self.__nome
+    def getCargaHoraria():
+        return self.__cargaHoraria
+    def getValor():
+        return self.__valor
+    def getProfessor():
+        return self._professor
+    
+    def setNome(self, nome):
+        self.__nome = nome
+    def setCargaHoraria(self,cargaHoraria):
+        self.__cargaHoraria = cargaHoraria
+    def setValor(self, valor):
+        self.__valor = valor
+    def setProfessor():
+        self.__professor = professor
+
+    def retornaValorHora():
+        total = self.__cargaHoraria
